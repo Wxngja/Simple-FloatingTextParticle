203ef626e138fe8d3880636bc62d6f98c9d6c91e
MyrulXzavier (GamerXzavier) <GamerXzavier@gmail.com>
2016-08-02 19:29:54 +0800

    basic php

---
+++ src/FloatingTextParticle/Main.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace FloatingTextParticle;
+
+//player
+use pocketmine\Player;
+//inventory
+use pocketmine\inventory\Inventory;
+use pocketmine\inventory\ChestInventory;
+use pocketmine\inventory\DoubleChestInventory;
+//events
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\entity\ProjectileHitEvent;
+use pocketmine\event\entity\EntityShootBowEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageByChildEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+use pocketmine\event\Listener;
+//items
+use pocketmine\item\Slimeball;
+use pocketmine\item\Item;
+//commands
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\command\ConsoleCommandSender;
+//utils
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\config;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\TextFormat as MT;
+use pocketmine\utils\TextFormat as C;
+use pocketmine\utils\BinaryStream;
+use pocketmine\utils\Binary;
+//entity
+use pocketmine\entity\Effect;
+use pocketmine\entity\Entity;
+//level
+use pocketmine\level\sound\BlazeShootSound;
+use pocketmine\level\sound\GhastShootSound;
+use pocketmine\level\sound\EndermanTeleportSound;
+use pocketmine\level\sound\AnvilFallSound;
+use pocketmine\level\particle\FloatingTextParticle;
+use pocketmine\level\particle\ExplodeParticle;
+use pocketmine\level\particle\BubbleParticle;
+use pocketmine\level\particle\Particle;
+use pocketmine\level\particle\DustParticle;
+use pocketmine\level\Position;
+use pocketmine\level\Location;
+use pocketmine\level\Position\getLevel;
+use pocketmine\level\format\Chunk;
+use pocketmine\level\format\FullChunk;
+use pocketmine\level\Level;
+//block
+use pocketmine\block\IronOre;
+use pocketmine\block\GoldOre;
+use pocketmine\block\Block;
+//plugin
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginManager;
+use pocketmine\plugin\Plugin;
+//server
+use pocketmine\Server;
+//network
+use pocketmine\network\protocol\AddEntityPacket;
+use pocketmine\network\protocol\PlayerActionPacket;
+use pocketmine\network\protocol\MovePlayerPacket;
+use pocketmine\network\protocol\BlockEventPacket;
+//math
+use pocketmine\math\Vector3;
+use pocketmine\math\Math;
+use pocketmine\math\AxisAlignedBB;
+//scheduler
+use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\CallbackTask;
+
+class Main extends PluginBase implements Listener{
+
+  public function onEnable(){
+    $this->getLogger()->info("§aPlugin Have Being Load.");
+
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+  }
+
+   public function onDisable(){
+       $this->getLogger()->info("§cPlugin Have Being Unload.");
+
+   }
+
+   public function addParticle(PlayerJoinEvent $event){ 
+
+       $level = $this->getServer()->getLevelByName("ArchLobby");
+
+       $player = $event->getPlayer();
+
+       $level->addParticle(new FloatingTextParticle(new Vector3(128.55, 20.55, 139.55),"", "§aWelcome Beginner!"));
+
+       $level->addParticle(new FloatingTextParticle(new Vector3(128.55, 20.10, 139.55),"", "§eYou Will Randomly Teleport"));
+
+       $level->addParticle(new FloatingTextParticle(new Vector3(128.55, 19.59, 139.55),"", "§eTo Random City When You Entered The Realms!"));
+
+       $level->addParticle(new FloatingTextParticle(new Vector3(128.55, 20.50, 145.55),"", "§dRealms"));
+
+       $level->addParticle(new FloatingTextParticle(new Vector3(123.55, 20.50, 139.55),"", "§bParkour"));
+
+       $level->addParticle(new FloatingTextParticle(new Vector3(134.55, 19.50, 139.55),"", "§cInformation"));
+
+   }
+
+}
